a384d5604ef9751650b9c440629cec5bde433db3
GideonMoises <isidrogideonmoises@gmail.com>
2026-01-23 16:30:11 +0800

    Update file_organizer.py
    -Added multiple file format
    -Improve the readability of the code

--- file_organizer.py
+++ file_organizer.py
@@ -1,26 +1,47 @@
 import os
 import shutil
 
-path = r"C:\Users\carbo\Downloads" 
-
+path = input("Input folder destination: ") 
 items = os.listdir(path)
 
-folders = ["folderImg", "folderDoc", "folderZip", "folderVid"]
+folders = {
+    ".png": "Images",
+    ".jpg": "Images",
+    ".jpeg": "Images",
+    ".tiff": "Images",
+    ".webp": "Images",
+    ".avif": "Images",
+    ".svg": "Images",
+    ".jfif": "Images",
+    ".JPG": "Images",
+    ".doc": "Document",
+    ".docx": "Document",
+    ".pdf": "Document",
+    ".txt": "Document",
+    ".xlsx": "Document",
+    ".xls": "Document",
+    ".pdf": "Pdf",
+    ".mp4": "Video",
+    ".m4v": "Video",
+    ".mov": "Video",
+    ".mkv": "Video",
+    ".exe": "Application",
+    ".zip": "Zip",
+    ".7z": "Zip",
+    ".rar": "Zip",
+    ".tar": "Zip",
+    ".ppt": "Presentation",
+    ".pptx": "Presentation",
+    ".ppsx": "Presentation",
+
+}
 
 for folder in folders:
-    full_folder = os.path.join(path, folder)
+    full_folder = os.path.join(path, folders[folder])
     os.makedirs(full_folder, exist_ok=True)
-
-for file in items:
-    full_path = os.path.join(path, file) 
-    if os.path.isfile(full_path):
-        name, ext = os.path.splitext(file)
-        print(ext)
-        if ext == ".png" or ext == ".jpeg" or ext == ".JPG" or ext == ".jfif" or ext == ".jpg":
-            shutil.move(full_path, os.path.join(path, folders[0]))
-        elif ext == ".docx" or ext == ".doc" or ext == ".pdf":
-             shutil.move(full_path, os.path.join(path, folders[1]))
-        elif ext == ".zip" or ext == ".7z" or ext == ".rar":
-            shutil.move(full_path, os.path.join(path, folders[2]))
-        elif ext == ".mp4":
-            shutil.move(full_path, os.path.join(path, folders[2]))
+    for file in items:
+        full_path = os.path.join(path, file)
+        if os.path.isfile(full_path):
+            name,ext = os.path.splitext(file)
+            if ext == (folder):
+                shutil.move(full_path,full_folder)
